6a88e942a0f809888572f0b2216d50108321d82a
joebanhong <joeanddaneil5@gmail.com>
2023-01-15 23:48:29 +0800

    feat: create categories container

--- src/component/CategoriesContainer/CategoriesContainer.js
+++ src/component/CategoriesContainer/CategoriesContainer.js
@@ -4,6 +4,7 @@ import CategoryContainer from "./CategoryContainer";
 const CategoriesContainer = () => {
   const categoriesList = ['Hats', 'Jackets', 'Sneakers','Women','Men']
   const categoryContainerJSx = categoriesList.map((category) => <CategoryContainer key={category} category={category}/>)
+  
   return (
     <ul className="categories-container">
       {categoryContainerJSx}
