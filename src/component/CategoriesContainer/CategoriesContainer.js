04b792033f9c675086a6ae1c72155d94caa04fef
joebanhong <joeanddaneil5@gmail.com>
2023-01-17 01:23:39 +0800

    feat: add custom font

--- src/component/CategoriesContainer/CategoriesContainer.js
+++ src/component/CategoriesContainer/CategoriesContainer.js
@@ -2,23 +2,6 @@ import React from "react";
 import CategoryContainer from "./CategoryContainer";
 import categoriesInfoList from './categories.json'
 const CategoriesContainer = () => {
-  // const categoriesInfoList = [
-  //   {
-  //     title: "Hats",
-  //   },
-  //   {
-  //     title: "Jackets",
-  //   },
-  //   {
-  //     title: "Sneakers",
-  //   },
-  //   {
-  //     title: "Women",
-  //   },
-  //   {
-  //     title: "Men",
-  //   },
-  // ];
   const categoryContainerJSx = categoriesInfoList.map((categoryInfo) => (
     <CategoryContainer key={categoryInfo.title} categoryInfo={categoryInfo} />
   ));
