2c569c4451756f842d931a952e61a1d7e041a9fc
joebanhong <joeanddaneil5@gmail.com>
2023-01-16 23:08:45 +0800

    feat: add pic

--- src/component/CategoriesContainer/CategoryContainer.js
+++ src/component/CategoriesContainer/CategoryContainer.js
@@ -1,12 +1,14 @@
 import React from "react";
 import './categoriesContainer.scss'
 
-const CategoryContainer = ({ category }) => {
+const CategoryContainer = ({ categoryInfo }) => {
+  const {title, imageUrl} = categoryInfo;
   return (
     <li className="category-container">
       {/* <img /> */}
+      <div className="background-image" style={{backgroundImage:`url(${imageUrl})`}}/>
       <div className="category-body-container">
-        <h2>{category}</h2>
+        <h2>{title}</h2>
         <p>Shop Now</p>
       </div>
     </li>
