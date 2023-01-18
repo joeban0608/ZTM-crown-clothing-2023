9e8d10ccb3898debaeae22f4d5818287a054c77c
joebanhong <joeanddaneil5@gmail.com>
2023-01-18 14:13:39 +0800

    feat: sign in 設置 - package 新增 firebase

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./routes/Home/Home";
 import Navigation from "./routes/Navigation/Navigation";
 import Shop from "./routes/Shop/Shop";
+import SignIn from "./routes/SignIn/SignIn";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path="/shop" element={<Shop />} />
+        <Route path="/sign-in" element={<SignIn />} />
       </Route>
     </Routes>
   );
