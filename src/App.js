e1c4aa24eb7df4c83ed2ea75d8047f933a664742
joebanhong <joeanddaneil5@gmail.com>
2023-01-25 16:43:22 +0800

    - add shopData - add ProductCard component - add button - add shoppingBag icon - add card dropdown component - add shop rtk - add toggle function for card dropdown - add function addCartItem - rename cart - add removeItemToCard - add arrow - add setCartTotal rtk

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import Authentication from "./routes/Authentication/Authentication";
+import Checkout from "./routes/Checkout/Checkout";
 import Home from "./routes/Home/Home";
 import Navigation from "./routes/Navigation/Navigation";
 import Shop from "./routes/Shop/Shop";
@@ -9,8 +10,9 @@ const App = () => {
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
-        <Route path="/shop" element={<Shop />} />
-        <Route path="/auth" element={<Authentication />} />
+        <Route path="shop" element={<Shop />} />
+        <Route path="auth" element={<Authentication />} />
+        <Route path="checkout" element={<Checkout />} />
       </Route>
     </Routes>
   );
