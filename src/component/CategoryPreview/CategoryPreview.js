d65cb79c3c17af55fe59486f48d3a556cdbed442
joebanhong <joeanddaneil5@gmail.com>
2023-01-28 17:35:34 +0800

    - add category link

--- src/component/CategoryPreview/CategoryPreview.js
+++ src/component/CategoryPreview/CategoryPreview.js
@@ -1,15 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import ProductCard from "../ProductCard/ProductCard";
 import "./categoryPreview.scss";
 
 const CategoryPreview = ({ title, products }) => {
-
   return (
     <div className="category-preview-container">
       {
         // 這邊用 h2 包 span 是為了只點擊 span 區塊, h2 為區塊元素,會整個 width 都點到
         <h2>
-          <span className="title">{title.toUpperCase()}</span>
+          <Link className="title" to={title}>
+            {title.toUpperCase()}
+          </Link>
         </h2>
       }
       <div className="preview">
