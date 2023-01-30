81916eaad8657467e2720c6480e24fd6187d8b1e
joebanhong <joeanddaneil5@gmail.com>
2023-01-30 17:18:50 +0800

    feat: 新增  redux persist 套件, 將 shop reducer 存入 loacl storage - add redux-persist

--- src/index.js
+++ src/index.js
@@ -5,17 +5,21 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import "./utils/resetCss.scss";
-import { store } from "./store/store";
+import { persistor, store } from "./store/store";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
 
--- src/store/store.js
+++ src/store/store.js
@@ -1,22 +1,33 @@
-import {
-  applyMiddleware,
-  compose,
-  createStore,
-} from "@reduxjs/toolkit";
+import { applyMiddleware, compose, createStore } from "@reduxjs/toolkit";
 
 import logger from "redux-logger";
 import { rootReducer } from "./rootReducer";
+import storage from "redux-persist/lib/storage";
+import persistReducer from "redux-persist/es/persistReducer";
+import persistStore from "redux-persist/es/persistStore";
+
+/*
+  透過 redux-persist 存入 localStorage 
+  blacklist  將你不想存入 local 的 rootReducer/reducer 寫入
+  這邊 black user 是因為, Auth 是透過監聽 firebase 去做改變, 不需要將 user 存入 local storage 
+*/
+const persistConfig = {
+  key: "root",
+  storage,
+  blacklist: ["user"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const middlewares = [logger];
+
 const composedEnhancers = compose(applyMiddleware(...middlewares));
-export const store = createStore(rootReducer, undefined, composedEnhancers);
-/* configureStore({
-  reducer: {
-    user: userReducer,
-    shop: shopReducer,
-  },
-  middleware: getDefaultMiddleware({
-    serializableCheck: false,
-  }).concat(logger),
-});
- */
+
+// export const store = createStore(rootReducer, undefined, composedEnhancers);
+export const store = createStore(
+  persistedReducer,
+  undefined,
+  composedEnhancers
+);
+
+export const persistor = persistStore(store);
